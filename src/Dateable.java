5da8b8bb0dbd5954d3a20091b117d7b1e2bbe0a0
Elise Sze <elisewks2027@student.cis.edu.hk>
2024-08-28 22:22:37 +0800

    Finished Lab 1 :)

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+    Scanner x = new Scanner(System.in);
+    System.out.println("Enter your age: ");
+    int age = x.nextInt();
+    int other = (7+(age/2));
+    System.out.println(age + "-year olds should date somebody who is at least " + other + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = x.nextInt();
+        int pages = (100-age);
+        System.out.println(age + "-year olds should read at least " + pages + " pages before giving up on a book.");
 
     }
 }
